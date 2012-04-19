eb643c333fe09053e34344af6cd832a1fb4afa03
nitroglycerine33 <nitroglycerine33@gmail.com>
2012-04-18 19:12:02 -0700

    Add IME keyboard switch toggle - Romanbb

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2193,6 +2193,12 @@ public final class Settings {
 	*/
 	public static final String VOLBTN_MUSIC_CONTROLS = "volbtn_music_controls";
 
+	/**
+	* Whether or not to display the IME selection in the statusbar pulldown notifications
+	* @hide
+	*/
+	public static final String SHOW_STATUSBAR_IME_SWITCHER = "show_statusbar_ime_switcher";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
