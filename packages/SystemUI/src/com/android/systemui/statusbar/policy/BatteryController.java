09465a527b13f930aeb83f38a877628e4bc22fab
nitroglycerine33 <nitroglycerine33@gmail.com>
2012-10-15 21:41:33 -0500

    Add CM Circle battery and remove circle mod battery.

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/BatteryController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/BatteryController.java
@@ -41,13 +41,21 @@ public class BatteryController extends BroadcastReceiver {
     private ArrayList<ImageView> mIconViews = new ArrayList<ImageView>();
     private ArrayList<TextView> mLabelViews = new ArrayList<TextView>();
 
-    private static final int BATTERY_STYLE_NORMAL  = 0;
-    private static final int BATTERY_STYLE_TEXT    = 1;
-    private static final int BATTERY_STYLE_CIRCLE  = 2;
-    private static final int BATTERY_STYLE_BAR     = 3;
-    private static final int BATTERY_STYLE_FATTY   = 4;
-    private static final int BATTERY_STYLE_DIGITAL = 5;
-    private static final int BATTERY_STYLE_GONE    = 6;
+    private static final int BATTERY_STYLE_NORMAL         = 0;
+    private static final int BATTERY_STYLE_PERCENT        = 1;
+    /***
+     * BATTERY_STYLE_CIRCLE* cannot be handled in this controller, since we cannot get views from
+     * statusbar here. Yet it is listed for completion and not to confuse at future updates
+     * See CircleBattery.java for more info
+     *
+     * set to public to be reused by CircleBattery
+     */
+    public  static final int BATTERY_STYLE_CIRCLE         = 2;
+    public  static final int BATTERY_STYLE_CIRCLE_PERCENT = 3;
+    private static final int BATTERY_STYLE_BAR     = 4;
+    private static final int BATTERY_STYLE_FATTY   = 5;
+    private static final int BATTERY_STYLE_DIGITAL = 6;
+    private static final int BATTERY_STYLE_GONE    = 7;
 
     private static final int BATTERY_ICON_STYLE_NORMAL         = R.drawable.stat_sys_battery;
     private static final int BATTERY_ICON_STYLE_CHARGE         = R.drawable.stat_sys_battery_charge;
@@ -55,8 +63,6 @@ public class BatteryController extends BroadcastReceiver {
     private static final int BATTERY_ICON_STYLE_CHARGE_MIN     = R.drawable.stat_sys_battery_charge_min;
     private static final int BATTERY_ICON_STYLE_NORMAL_BAR     = R.drawable.stat_sys_battery_bar;
     private static final int BATTERY_ICON_STYLE_CHARGE_BAR     = R.drawable.stat_sys_battery_charge_bar;
-    private static final int BATTERY_ICON_STYLE_NORMAL_CIRCLE  = R.drawable.stat_sys_battery_circle;
-    private static final int BATTERY_ICON_STYLE_CHARGE_CIRCLE  = R.drawable.stat_sys_battery_charge_circle;
     private static final int BATTERY_ICON_STYLE_NORMAL_FATTY   = R.drawable.stat_sys_battery_fatty;
     private static final int BATTERY_ICON_STYLE_CHARGE_FATTY   = R.drawable.stat_sys_battery_charge_fatty;
     private static final int BATTERY_ICON_STYLE_NORMAL_DIGITAL = R.drawable.stat_sys_battery_digital;
@@ -136,28 +142,24 @@ public class BatteryController extends BroadcastReceiver {
         int mText = View.GONE;
         int mIconStyle = BATTERY_ICON_STYLE_NORMAL;
 
-        if (mBatteryStyle == 0) {
+        if (mBatteryStyle == BATTERY_STYLE_NORMAL) {
             mIcon = (View.VISIBLE);
             mIconStyle = mBatteryPlugged ? BATTERY_ICON_STYLE_CHARGE
                     : BATTERY_ICON_STYLE_NORMAL;
-        } else if (mBatteryStyle == 1) {
+        } else if (mBatteryStyle == BATTERY_STYLE_PERCENT) {
             mIcon = (View.VISIBLE);
             mText = (View.VISIBLE);
             mIconStyle = mBatteryPlugged ? BATTERY_ICON_STYLE_CHARGE_MIN
                     : BATTERY_ICON_STYLE_NORMAL_MIN;
-        } else if (mBatteryStyle == 2) {
-            mIcon = (View.VISIBLE);
-            mIconStyle = mBatteryPlugged ? BATTERY_ICON_STYLE_CHARGE_CIRCLE
-                    : BATTERY_ICON_STYLE_NORMAL_CIRCLE;
-        } else if (mBatteryStyle == 3) {
+        } else if (mBatteryStyle == 4) {
             mIcon = (View.VISIBLE);
             mIconStyle = mBatteryPlugged ? BATTERY_ICON_STYLE_CHARGE_BAR
                     : BATTERY_ICON_STYLE_NORMAL_BAR;
-        } else if (mBatteryStyle == 4) {
+        } else if (mBatteryStyle == 5) {
             mIcon = (View.VISIBLE);
             mIconStyle = mBatteryPlugged ? BATTERY_ICON_STYLE_CHARGE_FATTY
                     : BATTERY_ICON_STYLE_NORMAL_FATTY;
-        } else if (mBatteryStyle == 5) {
+        } else if (mBatteryStyle == 6) {
  	    mIcon = (View.VISIBLE);
  	    mIconStyle = mBatteryPlugged ? BATTERY_ICON_STYLE_CHARGE_DIGITAL	
                     : BATTERY_ICON_STYLE_NORMAL_DIGITAL;
