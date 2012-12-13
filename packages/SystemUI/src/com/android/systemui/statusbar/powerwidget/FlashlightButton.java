efb31e52d09415925bee80e75c9623eb0dbb7bf8
nitroglycerine33 <nitroglycerine33@GrayFox.(none)>
2012-12-12 22:25:14 -0600

    Fix torch after merge

--- packages/SystemUI/src/com/android/systemui/statusbar/powerwidget/FlashlightButton.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/powerwidget/FlashlightButton.java
@@ -6,12 +6,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.provider.Settings;
+import android.util.Log;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FlashlightButton extends PowerButton {
 
+    private Torch mTorch;
     private static final List<Uri> OBSERVED_URIS = new ArrayList<Uri>();
     static {
         OBSERVED_URIS.add(Settings.System.getUriFor(Settings.System.TORCH_STATE));
@@ -33,22 +36,28 @@ public class FlashlightButton extends PowerButton {
 
     @Override
     protected void toggleState(Context context) {
-        boolean bright = Settings.System.getInt(context.getContentResolver(),
-                Settings.System.EXPANDED_FLASH_MODE, 0) == 1;
-        Intent i = new Intent("net.cactii.flash2.TOGGLE_FLASHLIGHT");
-        i.putExtra("bright", bright);
-        context.sendBroadcast(i);
+        boolean torchOn = (mState == STATE_ENABLED);
+        if (torchOn) {
+        	Settings.System.putInt(context.getContentResolver(), Settings.System.TORCH_STATE, 0);
+        	mTorch = Torch.getTorch();
+            mTorch.finish();
+        } else {
+        	Settings.System.putInt(context.getContentResolver(), Settings.System.TORCH_STATE, 1);
+        	Intent i = new Intent(context, Torch.class);
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
+        }
     }
 
     @Override
     protected boolean handleLongClick(Context context) {
         // it may be better to make an Intent action for the Torch
         // we may want to look at that option later
-        Intent intent = new Intent(Intent.ACTION_MAIN);
-        intent.setClassName("net.cactii.flash2", "net.cactii.flash2.MainActivity");
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(intent);
-        return true;
+        // Intent intent = new Intent(Intent.ACTION_MAIN);
+        // intent.setClassName("net.cactii.flash2", "net.cactii.flash2.MainActivity");
+        // intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        // context.startActivity(intent);
+        return false;
     }
 
     @Override
